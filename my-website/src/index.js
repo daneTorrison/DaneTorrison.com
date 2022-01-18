7cae8ddd681438becb826de76e5c9e7fe6debdb4
daneTorrison <danetorrison2@gmail.com>
2022-01-18 13:38:06 -0800

    new

--- my-website/src/index.js
+++ my-website/src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Homepage(){
+  return (
+    <h1>Dane is coool</h1>
+  );
+}
+const yell = <Homepage/>;
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  yell,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
